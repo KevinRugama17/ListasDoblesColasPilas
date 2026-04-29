68ceee1f5b6e6627556f5cbd642d7ab26f3d387e
Kevin Rugama17 <kevinrugama171@gmail.com>
2026-04-29 14:38:12 -0600

    Commit de los primeros detalles

--- src/practica1listasdoblescolaspilas/Ejercicio2/Ejercicio2.java
+++ src/practica1listasdoblescolaspilas/Ejercicio2/Ejercicio2.java
@@ -4,17 +4,53 @@
  */
 package practica1listasdoblescolaspilas.Ejercicio2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kevin
  */
 public class Ejercicio2 {
 
-    /**
-     * @param args the command line arguments
-     */
+static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+   
+     
+        int opcion;
+        do {
+            System.out.println("\n-- Soporte tecnico --");
+            System.out.println("");
+            System.out.println("1. Encolar nueva solicitud");
+            System.out.println("2. desencolar la siguiente solicitud en espera");
+            System.out.println("3. Mostrar todos los prestamos activos ( (inicio -> fin)");
+            System.out.println("4. Mostrar todos los prestamos activos ( (fin -> inicio)");
+            System.out.println("5. Buscar un libro por su titulo o codigo");
+            System.out.println("6. Buscar un libro por su titulo o codigo");
+            System.out.println("0. Salir");
+            System.out.print("Opcion: ");
+            opcion = leerInt();
+            switch (opcion) {
+           /*     case 1:
+                    System.out.print("Nueva solicitud: ");
+                    historial.visitarPagina(sc.nextLine().trim());
+                    break;
+                case 2: historial.retroceder(); break;
+                case 3: historial.avanzar();    break;
+                case 4: historial.mostrarAdelanteAtras(); break;
+                case 5: historial.mostrarAtrasAdelante(); break;
+                case 0: System.out.println("Saliendo..."); break;
+                default: System.out.println("Opcion invalida.");*/
+            }
+        } while (opcion != 0);
+        sc.close();
+    }
+ 
+    static int leerInt() {
+        try { 
+            return Integer.parseInt(sc.nextLine().trim());
+        } catch (NumberFormatException e) 
+        { return -1; }
     }
     
 }
